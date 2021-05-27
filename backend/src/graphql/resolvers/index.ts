dcb0e012cd74d57cae36519e93b72e784ccac61a
Vitor Sanches <viisanches98@gmail.com>
2021-05-27 20:50:20 -0300

    Fix graphql resolvers and types pathfiles

--- backend/src/graphql/resolvers/index.ts
+++ backend/src/graphql/resolvers/index.ts
@@ -2,7 +2,7 @@ import { mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import path from 'path';
 
-const resolversArray = loadFilesSync(path.join(__dirname, '**', 'resolver.ts'));
+const resolversArray = loadFilesSync(path.join(__dirname, './', '**', '*.resolver.ts'));
 const resolvers = mergeResolvers(resolversArray);
 
 export default resolvers;
--- backend/src/graphql/types/index.ts
+++ backend/src/graphql/types/index.ts
@@ -2,7 +2,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import path from 'path';
 
-const typesArray = loadFilesSync(path.join(__dirname, '**', '*.gql'));
+const typesArray = loadFilesSync(path.join(__dirname, './', '**', '*.gql'));
 const typeDefs = mergeTypeDefs(typesArray);
 
 export default typeDefs;
